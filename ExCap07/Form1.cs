45b7c714c4582439ca941fed9f6f0e95f544cd02
Roberto Hoo <prof.robertohoo@gmail.com>
2017-08-22 04:10:03 -0300

    tudo ok novamente

--- ExCap07/Form1.cs
+++ ExCap07/Form1.cs
@@ -16,5 +16,25 @@ namespace ExCap07
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            displayListBox.Items.Add(inputTextBox.Text);
+            inputTextBox.Clear();
+            inputTextBox.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // check whether item is selected; if so, remove
+            if (displayListBox.SelectedIndex != -1)
+                displayListBox.Items.RemoveAt(displayListBox.SelectedIndex);
+            // end method removeButton_Click
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            displayListBox.Items.Clear();
+        }
     }
 }

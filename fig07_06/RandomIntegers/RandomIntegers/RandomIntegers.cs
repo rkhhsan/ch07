ab58d58ff414749f7e5a975188e2f0e16b2e0e5c
Roberto Kai Hsi Hoo <prof.robertohoo@gmail.com>
2020-04-22 20:30:30 -0300

    ok

--- fig07_06/RandomIntegers/RandomIntegers/RandomIntegers.cs
+++ fig07_06/RandomIntegers/RandomIntegers/RandomIntegers.cs
@@ -8,9 +8,11 @@ public class RandomIntegers
    {
       Random randomNumbers = new Random(); // random number generator
       int face; // stores each random integer generated
+      int qt = 100;
 
       // loop 20 times
-      for (int counter = 1; counter <= 20; counter++)
+      Console.WriteLine("{0} random integers from 1 to 6:",qt);
+      for (int counter = 1; counter <= 100; counter++)
       {
          // pick random integer from 1 to 6
          face = randomNumbers.Next(1, 7);
@@ -18,9 +20,50 @@ public class RandomIntegers
          Console.Write("{0}  ", face); // display generated value
 
          // if counter is divisible by 5, start a new line of output
-         if (counter % 5 == 0)
-            Console.WriteLine();
+         if (counter % 10 == 0)  Console.WriteLine();
       } // end for
+
+      Random rnd = new Random();
+
+      Console.WriteLine("\n{0} random integers from -100 to 100:", qt);
+      for (int ctr = 1; ctr <= qt; ctr++)
+      {
+         Console.Write("{0,4}", rnd.Next(-100, 101));
+         if (ctr % 10 == 0) Console.WriteLine();
+      }
+
+      Console.WriteLine("\n{0} random integers from 1000 to 10000:", qt);
+      for (int ctr = 1; ctr <= qt; ctr++)
+      {
+         Console.Write("{0,6}", rnd.Next(1000, 10001));
+         if (ctr % 10 == 0) Console.WriteLine();
+      }
+
+      Console.WriteLine("\n{0} random integers from 1 to 10:", qt);
+      for (int ctr = 1; ctr <= qt; ctr++)
+      {
+         Console.Write("{0,3}", rnd.Next(1, 11));
+         if (ctr % 10 == 0) Console.WriteLine();
+      }
+
+      // The example displays output similar to the following:
+      //       20 random integers from -100 to 100:
+      //           65   -95   -10    90   -35
+      //          -83   -16   -15   -19    41
+      //          -67   -93    40    12    62
+      //          -80   -95    67   -81   -21
+      //       
+      //       20 random integers from 1000 to 10000:
+      //           4857    9897    4405    6606    1277
+      //           9238    9113    5151    8710    1187
+      //           2728    9746    1719    3837    3736
+      //           8191    6819    4923    2416    3028
+      //       
+      //       20 random integers from 1 to 10:
+      //            9     8     5     9     9
+      //            9     1     2     3     8
+      //            1     4     8    10     5
+      //            9     7     9    10     5
 
       Console.WriteLine("\nTecle qualquer tecla para finalizar...");
       Console.ReadKey();

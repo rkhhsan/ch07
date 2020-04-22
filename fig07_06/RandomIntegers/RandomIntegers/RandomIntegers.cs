83bbbecf9756c8166a95fb7f8409d0f94221cf44
Roberto Kai Hsi Hoo <prof.robertohoo@gmail.com>
2020-04-22 20:10:30 -0300

    ok

--- fig07_06/RandomIntegers/RandomIntegers/RandomIntegers.cs
+++ fig07_06/RandomIntegers/RandomIntegers/RandomIntegers.cs
@@ -4,23 +4,26 @@ using System;
 
 public class RandomIntegers
 {
-   public static void Main( string[] args )
+   public static void Main(string[] args)
    {
       Random randomNumbers = new Random(); // random number generator
       int face; // stores each random integer generated
 
       // loop 20 times
-      for ( int counter = 1; counter <= 20; counter++ )
+      for (int counter = 1; counter <= 20; counter++)
       {
          // pick random integer from 1 to 6
-         face = randomNumbers.Next( 1, 7 );
+         face = randomNumbers.Next(1, 7);
 
-         Console.Write( "{0}  ", face ); // display generated value
+         Console.Write("{0}  ", face); // display generated value
 
          // if counter is divisible by 5, start a new line of output
-         if ( counter % 5 == 0 )
+         if (counter % 5 == 0)
             Console.WriteLine();
       } // end for
+
+      Console.WriteLine("\nTecle qualquer tecla para finalizar...");
+      Console.ReadKey();
    } // end Main
 } // end class RandomIntegers
 
